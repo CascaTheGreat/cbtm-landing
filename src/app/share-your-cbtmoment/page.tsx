f793577353efc546472d517820299af759c1b6ea
Leo Ledlow <jledlow22@gmail.com>
2026-07-17 22:42:13 -0400

    Fixed suspense bug

--- src/app/share-your-cbtmoment/page.tsx
+++ src/app/share-your-cbtmoment/page.tsx
@@ -1,10 +1,13 @@
 import Container from "@/components/Container";
 import Form from "@/components/Form";
+import { Suspense } from "react";
 
 export default function ShareYourCbtmoment() {
   return (
     <Container className="pt-32 pb-16 lg:pt-40 lg:pb-24">
-      <Form />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Form />
+      </Suspense>
     </Container>
   );
 }
