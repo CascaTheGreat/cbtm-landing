27b274de5127b4b6eec6661da5f125081a47a44c
Leo Ledlow <jledlow22@gmail.com>
2026-08-23 10:31:59 -0400

    Update CTA

--- src/data/cta.ts
+++ src/data/cta.ts
@@ -2,6 +2,6 @@ export const ctaDetails = {
   heading: "Join the Next Generation of College Social Life",
   subheading:
     "Always be in the know with CBTM. Discover events, connect with friends, and explore your campus like never before.",
-  appStoreUrl: process.env.APPSTORE_URL || "#",
-  googlePlayUrl: process.env.GOOGLE_PLAY_URL || "#",
+  appStoreUrl: process.env.NEXT_PUBLIC_APPSTORE_URL || "#",
+  googlePlayUrl: process.env.NEXT_PUBLIC_GOOGLE_PLAY_URL || "#",
 };
